78b3aa2b8cc6dce82a86e7136e52a3ae9578d7b4
RR1335 <odyli@126.com>
2025-02-28 08:24:34 -0500

    Iterator ES6

---
+++ design-pattern/iterator-es6.js
@@ -0,0 +1,11 @@
+function each(data) {
+    let iterator = data[Symbol.iterator]()
+
+    let item = {done:false}
+    while (!item.done) {
+        item = iterator.next()
+        if (!item.done) {
+            console.log(item.value)
+        }
+    }
+}
